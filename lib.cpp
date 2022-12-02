3d845785f0820f1592693bb66abc39e5352c582a
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-02 10:15:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool primo (int n, int num) {
+int q = n % num;
+bool opzione = true;
+if (n>1) {
+if (q != 0) {
+opzione = primo (n, num+1);
+} else if ((q == 0) && (n != num)) {
+return false;
+} else if ((q == 0) && (n == num)) {
+return true;
+}
+return opzione;
+} else {
+return false;
+}
+}
