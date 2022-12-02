239b1118aac66495f6fdfe5b3e90d2d79b349279
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-02 10:12:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+  int n = 0, num = 2;
+cin >> n;
+if (primo(n, num)){
+cout << "numero primo" << endl;
+} else {
+cout << "numero non primo" << endl;
+}
   return 0;
 }
